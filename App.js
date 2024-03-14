67b23e0436d35a5df88caf7e9c62134aefbc9966
0anajuliaf <ana.santos3092@etec.sp.gov.br>
2024-03-14 08:32:21 -0300

    button

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Ana Júlia</Text>
       <Text>Anna Beatriz</Text>
+      <Text>14-03-2024</Text>
+      <title/>
+      <button tittle='Salvar'></button>
       <StatusBar style="auto" />
     </View>
   );
